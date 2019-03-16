e08c8bb39fc7750c9a9eb5b86a6fdbd17194a5fc
oleytterdahl <46402908+oleytterdahl@users.noreply.github.com>
2019-03-16 14:59:41 +0100

    Update config.php

--- futallaby040103/config.php
+++ futallaby040103/config.php
@@ -1,11 +1,11 @@
 <?
-define(TITLE, 'Futallaby-powered image board');		//Name of this image board
-define(SQLLOG, 'CHANGEME');		//Table (NOT DATABASE) used by image board
+define(TITLE, 'Yiff Dungeon Chan');		//Ekkeberg Dungeon
+define(SQLLOG, '1ac1');		//Table (NOT DATABASE) used by image board
 define(SQLHOST, 'localhost');		//MySQL server address, usually localhost
-define(SQLUSER, 'CHANGEME');		//MySQL user (must be changed)
-define(SQLPASS, 'CHANGEME');		//MySQL user's password (must be changed)
-define(SQLDB, 'CHANGEME');		//Database used by image board
-define(ADMIN_PASS, 'CHANGEME');	//Janitor password  (CHANGE THIS YO)
+define(SQLUSER, 'phazzedo_Phazzed');		//MySQL user (must be changed)
+define(SQLPASS, 'password123');		//MySQL user's password (must be changed)
+define(SQLDB, 'phazzedo_1ac1');		//Database used by image board
+define(ADMIN_PASS, 'jegelskerkuk');	//Janitor password  (CHANGE THIS YO)
 define(SHOWTITLETXT, '1');		//Show TITLE at top (1: yes  0: no)
 define(SHOWTITLEIMG, '0');		//Show image at top (0: no, 1: single, 2: rotating)
 define(TITLEIMG, 'title.jpg');	//Title image (point to php file if rotating)
@@ -27,7 +27,7 @@ define(MAX_RES, '30');		//Maximum topic bumps
 define(USE_THUMB, 1);		//Use thumbnails (1: yes  0: no)
 define(PROXY_CHECK, 0);		//Enable proxy check (1: yes  0: no)
 define(DISP_ID, 0);		//Display user IDs (1: yes  0: no)
-define(BR_CHECK, 15);		//Max lines per post (0 = no limit)
+define(BR_CHECK, 15);		//Max lines per post (25 = no limit)
 //NEW FOR FUTALLABY 040103
 define(TRIPKEY, '#');		//this character is displayed before tripcodes
 define(CSSFILE, 'futaba.css');	//location of the css file
